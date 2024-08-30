7fcd18ac9480223f9ae3b1136521d2c4c005e6ec
sreedev <xreedev@gmail.com>
2024-08-30 23:43:28 +0530

    added default input box

---
+++ FollowSyncReact/follow-sync/src/UsernameInput/UsernameInput.js
@@ -0,0 +1,12 @@
+import { Input, Stack } from '@chakra-ui/react'
+
+function UserNameInput(){
+  return(
+    <Stack spacing={3}>
+  <Input variant='filled' placeholder='Filled' />
+</Stack>
+  );
+};
+
+export default UserNameInput;
+
